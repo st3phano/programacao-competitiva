51df1deea9825d5056b50495c5da222ff0c71777
st3phano <stephano-@hotmail.com>
2023-05-28 19:00:35 -0300

    task description

---
+++ class02/deque.cpp
@@ -0,0 +1,15 @@
+/**
+ * Task: Create an application for a restaurant to manage customer orders.
+ * The application should allow customers to choose whether their orders
+ * will be delivered first (as a VIP order) or added to the normal queue.
+ */
+
+#include <deque>
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+   return 0;
+}
